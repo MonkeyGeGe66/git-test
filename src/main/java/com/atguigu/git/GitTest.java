056b57d6045278e6eb441d2b4eb48eccf51e9623
Monkey <Monkey@qq.com>
2022-03-12 17:40:18 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author houmingjiang
+ * @create 2022-03-12 17:34
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
